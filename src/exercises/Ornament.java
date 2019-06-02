af83b82dd91ca349e048a3cc08989cad4b6925db
bobanpesic <bobanpesic10gmail.com>
2019-06-02 12:48:39 +0200

    second change

--- src/exercises/Ornament.java
+++ src/exercises/Ornament.java
@@ -3,13 +3,24 @@ package exercises;
 /* 
  * Create a class called Decorator that contains an ArrayList called boxOfDecorations.
  *   1. In the Decorator class, hang all the ornaments in the box.
- *   2. Add a color to the Ornament class so that your program prints “Hanging a red ornament”, etc.
+ *   2. Add a color to the Ornament class so that your program prints Hanging a red ornament, etc.
  */
 
 public class Ornament {
-	
-	public void hang(){
+
+	private String ornamentColour;
+
+	public Ornament(String color) {
+		ornamentColour = color;
+	}
+
+	public void hang() {
 		System.out.println("hanging an ornament");
+	}
+
+	public String getColour() {
+		// TODO Auto-generated method stub
+		return ornamentColour;
 	}
 
 }
