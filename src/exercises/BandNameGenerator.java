735acdf78a7b67e0a410dc0b9353872dc2f36bf2
bobanpesic <bobanpesic10gmail.com>
2019-06-02 12:47:31 +0200

    second change

--- src/exercises/BandNameGenerator.java
+++ src/exercises/BandNameGenerator.java
@@ -1,5 +1,9 @@
 package exercises;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /* 
  * Make 2 ArrayLists; 
  *      1 full of adjectives (e.g. slimy, blind)
@@ -9,4 +13,42 @@ package exercises;
 */
 public class BandNameGenerator {
 
+	public static void main(String[] args) {
+
+		List<String> adjectives = new ArrayList<>();
+
+		adjectives.add("Crazy");
+		adjectives.add("Loud");
+		adjectives.add("Smeling");
+
+		List<String> nouns = new ArrayList<>();
+
+		nouns.add("dogs!");
+		nouns.add("Wizards!");
+		nouns.add("duck!");
+
+		BandNameGenerator bandName = new BandNameGenerator();
+
+		System.out.println("My band is called: " + (bandName.getRandomElement(adjectives)) + " "
+				+ (bandName.getRandomElement(nouns)));
+
+	}
+
+	public String getRandomElement(List<String> adjectives)
+
+	{
+
+		Random r = new Random();
+		return adjectives.get(r.nextInt(adjectives.size()));
+
+	}
+
+	public String getRandomElement1(List<String> nouns)
+
+	{
+
+		Random r1 = new Random();
+		return nouns.get(r1.nextInt(nouns.size()));
+
+	}
 }
